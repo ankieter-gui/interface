a757405471a5eb837a334325d91c2970dc73f76e
emil <emilm7843@gmail.com>
2021-06-10 12:34:09 +0200

    fixy

--- src/app/link-activator/link-activator.component.ts
+++ src/app/link-activator/link-activator.component.ts
@@ -12,8 +12,17 @@ export class LinkActivatorComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router:Router, private sharing:SharingService) { }
 
   ngOnInit(): void {
-   this.sharing.activateLink(this.route.snapshot.paramMap.get('hash')).subscribe(d=>
-   console.log(d))
+   this.sharing.activateLink(this.route.snapshot.paramMap.get('hash')).subscribe((d:any)=>{
+   if (!d.message){
+     if (d.object == "report"){
+       if (d.permission == "w") this.router.navigate(['/reports/editor/', d.id])
+       if (d.permission == "r") this.router.navigate(['/reports/', d.id])
+     }
+     else if (d.object == "survey"){
+
+     }
+
+   }});
   }
 
 }
