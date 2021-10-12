eddf81426fabd39d36feb3c2c12e95545d1021bb
emil <emilm7843@gmail.com>
2021-10-12 19:49:44 +0200

    isDevMode

--- src/app/Configuration.ts
+++ src/app/Configuration.ts
@@ -1,9 +1,11 @@
-export const isProd=false;
+import {isDevMode} from '@angular/core';
 
-const myIp = "localhost"
+export const isProd = isDevMode();
+
+const myIp = 'localhost';
 
 
-export const BACKEND_URL = !isProd?`http://${myIp}:5000/api`:"/api"
-export const FRONTEND_URL = !isProd?`http://${myIp}:4200`:"https://ankieter.projektstudencki.pl"
-export const LOGIN_SERVICE_URL = !isProd?`http://${myIp}:5000/api/login`:"/api/login"
+export const BACKEND_URL = !isProd ? `http://${myIp}:5000/api` : '/api';
+export const FRONTEND_URL = !isProd ? `http://${myIp}:4200` : 'https://ankieter.projektstudencki.pl';
+export const LOGIN_SERVICE_URL = !isProd ? `http://${myIp}:5000/api/login` : '/api/login';
 
